467d77a659f79cefa132908f34fad30ca9ff8c43
Lloyd <lloydmeta@gmail.com>
2017-04-11 01:25:04 +0900

    Add doctests for Scalaz version

--- scalaz/src/main/scala/diesel/implicits/Monadic.scala
+++ scalaz/src/main/scala/diesel/implicits/Monadic.scala
@@ -7,9 +7,66 @@ import scala.language.higherKinds
 import scala.language.implicitConversions
 
 /**
-  * Implicity converts DSLs to MonadicDSl so that you can use them in for-comprehensions.
+  * Implicitly converts DSLs to MonadicDSl so that you can use them in for-comprehensions.
   *
   * The catch is that the interpreter that you use at the end must be written for a Monad
+  *
+  * Example:
+  *
+  * {{{
+  * scala> import _root_.diesel._
+  *
+  * // Wrapper is only for the sake of sbt-doctest and is unnecessary in real-life usage
+  * scala> object Wrapper {
+  *      | // Declare our DSLs
+  *      | @diesel
+  *      | trait Maths[G[_]] {
+  *      |   def int(i: Int): G[Int]
+  *      |   def add(l: G[Int], r: G[Int]): G[Int]
+  *      | }
+  *      | @diesel
+  *      |  trait Applicative[F[_]] {
+  *      |    def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C]
+  *      |    def pure[A](a: A): F[A]
+  *      | } }
+  *
+  * // Import the stuff we've just built
+  * scala> import Wrapper._
+  * scala> import Maths._
+  * scala> import Applicative._
+  * scala> import scalaz.Monad
+  * scala> import scalaz.Scalaz._
+  *
+  * // Our combined algebra type and our program that uses it
+  * scala> type PRG[A[_]] = Applicative.Algebra[A] with Maths.Algebra[A]
+  * scala> val op = { (a: Int, b: Int, c: Int) =>
+  *      |    import monadic._
+  *      |    // Note the use of for comprehensions in here
+  *      |    for {
+  *      |      i <- add(int(a), int(b)).withAlg[PRG]
+  *      |      j <- pure(c).withAlg[PRG]
+  *      |      k <- add(int(i), int(j)).withAlg[PRG]
+  *      |    } yield k
+  *      | }
+
+  * // Write our interpreter
+  * scala> implicit def interp[F[_]](implicit F: Monad[F]) = new Applicative.Algebra[F] with Maths.Algebra[F] {
+  *      |    def int(i: Int) = F.pure(i)
+  *      |    def add(l: F[Int], r: F[Int]) =
+  *      |      for {
+  *      |        x <- l
+  *      |        y <- r
+  *      |      } yield x + y
+  *      |    def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = F.apply2(fa, fb)(f)
+  *      |    def pure[A](a: A): F[A] = F.pure(a)
+  *      | }
+  *
+  * // Now we can use our DSL
+  * scala> val program = op(1, 2, 3)
+  *
+  * scala> program[Option]
+  * res0: Option[Int] = Some(6)
+  * }}}
   */
 object monadic extends monadic
 
--- scalaz/src/main/scala/diesel/implicits/MonadicPlus.scala
+++ scalaz/src/main/scala/diesel/implicits/MonadicPlus.scala
@@ -6,10 +6,72 @@ import diesel.Dsl
 import scala.language.higherKinds
 
 /**
-  * Implicity converts DSLs to MonadicPlusDsl so that you can use them in for-comprehensions.
+  * Implicitly converts DSLs to MonadicPlusDsl so that you can use them in for-comprehensions.
   *
   * Note, this is more powerful than importing from monadic because this requires a MonadPlus instance
   * for the F[_] in the eventual interpreter that you use to get your results.
+  *
+  * Example:
+  *
+  * {{{
+  * scala> import _root_.diesel._
+  *
+  * // Wrapper is only for the sake of sbt-doctest and is unnecessary in real-life usage
+  * scala> object Wrapper {
+  *      | // Declare our DSLs
+  *      | @diesel
+  *      | trait Maths[G[_]] {
+  *      |   def int(i: Int): G[Int]
+  *      |   def add(l: G[Int], r: G[Int]): G[Int]
+  *      | }
+  *      | @diesel
+  *      |  trait Applicative[F[_]] {
+  *      |    def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C]
+  *      |    def pure[A](a: A): F[A]
+  *      | } }
+  *
+  * // Import the stuff we've just built
+  * scala> import Wrapper._
+  * scala> import Maths._
+  * scala> import Applicative._
+  * scala> import scalaz.Monad
+  * scala> import scalaz.Scalaz._
+  *
+  * // Our combined algebra type and our program that uses it
+  * scala> type PRG[A[_]] = Applicative.Algebra[A] with Maths.Algebra[A]
+  * scala> val op = { (a: Int, b: Int, c: Int) =>
+  *      |    import monadicplus._
+  *      |    // Note the use of for comprehensions in here
+  *      |    for {
+  *      |      i <- add(int(a), int(b)).withAlg[PRG]
+  *      |      if i > 3
+  *      |      j <- pure(c).withAlg[PRG]
+  *      |      k <- add(int(i), int(j)).withAlg[PRG]
+  *      |    } yield k
+  *      | }
+
+  * // Write our interpreter
+  * scala> implicit def interp[F[_]](implicit F: Monad[F]) = new Applicative.Algebra[F] with Maths.Algebra[F] {
+  *      |    def int(i: Int) = F.pure(i)
+  *      |    def add(l: F[Int], r: F[Int]) =
+  *      |      for {
+  *      |        x <- l
+  *      |        y <- r
+  *      |      } yield x + y
+  *      |    def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = F.apply2(fa, fb)(f)
+  *      |    def pure[A](a: A): F[A] = F.pure(a)
+  *      | }
+  *
+  * // Now we can use our DSL
+  * scala> val program1 = op(1, 2, 3)
+  * scala> val program2 = op(4, 5, 6)
+  *
+  * scala> program1[Option]
+  * res0: Option[Int] = None
+  *
+  * scala> program2[Option]
+  * res1: Option[Int] = Some(15)
+  * }}}
   */
 object monadicplus extends monadicplus
 
